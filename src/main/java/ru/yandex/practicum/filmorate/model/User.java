fe1cf530eeb9d0f47f24378b39e1dd1eafa4dff2
User Sholokhova <ekaterinasolohova29@gmail.com>
2024-06-19 14:20:40 +0300

    учтены замечания

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.Data;
 
@@ -14,11 +13,9 @@ public class User {
     String name;
     Long id;
     Set<Long> friends;
-    @NotNull
     @NotBlank
     @Email
     String email;
-    @NotNull
     @NotBlank
     String login;
     @Past
